3a78167b48757c0b557a4f512d69bd942213127f
Svetlana <svdorohova@gmail.com>
2022-09-05 14:12:59 +0300

    Ex03

---
+++ Ex_03/Program.cs
@@ -0,0 +1,19 @@
+﻿double[] cubes(int a)
+{
+    double[] x = new double[a];
+    for (int i = 0; i < a; i++)
+    {
+        x[i] = Math.Pow(i + 1, 3);
+    }
+    return x;
+}
+
+string output(double[] a)
+{
+
+    return string.Join(", ", a);
+}
+
+System.Console.WriteLine("Введите число: ");
+int polz = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(output(cubes(polz)));
