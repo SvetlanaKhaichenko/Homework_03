ba679b8516f656d89304d663f27672b4b2df0ee0
Svetlana <svdorohova@gmail.com>
2022-09-05 14:10:08 +0300

    Ex01

---
+++ Ex_01/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] mass (int []x, int a)
+{
+    for (int i = x.Length - 1; i >= 0; i--)
+{
+    x [i] = a % 10;
+    a = a / 10;
+}
+return x;
+}
+
+
+Console.WriteLine("Введите число из шести цифр");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int [] mass1 = new int [6];
+mass (mass1, a);
+
+if (mass1 [5] == mass1[0] && mass1[4] == mass1 [1] && mass1[3] == mass1[2])
+{
+    Console.WriteLine ("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine ("Число не является палиндромом");
+}
